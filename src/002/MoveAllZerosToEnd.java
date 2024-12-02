bc2a4c4fef2186c8401b3b904e252f2a58445769
AVINASH SUTHAR <avinashsuthar19hmh@gmail.com>
2024-12-02 19:31:52 +0530

    Day 2

--- src/002/MoveAllZerosToEnd.java
+++ src/002/MoveAllZerosToEnd.java
@@ -3,5 +3,28 @@ public class MoveAllZerosToEnd {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }    
-    
+    // User function Template for Java
+
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+   
+  
+        int n = arr.length;
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+
+        while (index < n) {
+            arr[index] = 0;
+            index++;
+        }
+
+    }
+}
 }
